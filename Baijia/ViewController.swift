8374be3c0aa2a0b0fd4fa198ab77fb801880369d
mac <mac@clover.local>
2015-11-10 22:38:39 +0800

    Initial Commit

---
+++ Baijia/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Baijia
+//
+//  Created by mac on 11/10/15.
+//  Copyright © 2015 mac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
